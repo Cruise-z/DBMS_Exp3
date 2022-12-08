9cb389ede1bf56d06b9506be1e5a218d795117fc
ZrzKIM <1983918430@qq.com>
2022-12-08 17:07:23 +0800

    ~

--- Database_back/Run.py
+++
@@ -1,8 +0,0 @@
-from init import *
-from data import *
-from create_view import *
-
-if __name__ == "__main__":
-    init_DB()
-    insert_data()
-    create_view()
--- 数据库爬虫全指南/python文件/Database/test1.py
+++
@@ -1,5 +0,0 @@
-import re
-a=' 45.00元'
-price_path = str(re.findall(r'.*?(\d+.\d{2})元', a)[0])
-print(price_path)
-
